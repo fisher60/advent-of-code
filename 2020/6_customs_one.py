36e28778385999ebb1fa9b1d6dfbb0fc2668edaf
fisher60 <wakefollower@gmail.com>
2020-12-05 23:50:26 -0800

    update inputs to be a fancier one-liner

--- 2020/6_customs_one.py
+++ 2020/6_customs_one.py
@@ -1,4 +1,4 @@
 with open("6_customs.txt", "r") as f:
-    inputs = list(map(lambda x: x.split("\n"), f.read().split("\n\n")))
+    inputs = list(map(lambda x: "".join(x.split("\n")), f.read().split("\n\n")))
 
-print(sum(len(set("".join(each))) for each in inputs))
+print(sum(len(set(each)) for each in inputs))
